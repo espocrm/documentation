893685d60884cc91b55470f7933eec48c3e566b4
Taras Machyshyn <taras@espocrm.com>
2021-09-16 13:11:48 +0300

    Changed the backup script due to changes in v.7

--- docs/_static/scripts/backup.sh
+++ docs/_static/scripts/backup.sh
@@ -7,10 +7,10 @@ if [ -z "$1" ]; then
     echo "Enter a full path to EspoCRM directory ($DEFAULT_PATH_TO_ESPO):"
     read PATH_TO_ESPO
     if [ -z "$PATH_TO_ESPO" ]; then
-        PATH_TO_ESPO=$DEFAULT_PATH_TO_ESPO
+        PATH_TO_ESPO="$DEFAULT_PATH_TO_ESPO"
     fi
 else
-    PATH_TO_ESPO=$1
+    PATH_TO_ESPO="$1"
 fi
 
 if [ ! -d "$PATH_TO_ESPO" ]; then
@@ -27,10 +27,10 @@ if [ -z "$2" ]; then
     echo "Enter a full path to backup directory ($DEFAULT_BACKUP_PATH):"
     read BACKUP_PATH
     if [ -z "$BACKUP_PATH" ]; then
-        BACKUP_PATH=$DEFAULT_BACKUP_PATH
+        BACKUP_PATH="$DEFAULT_BACKUP_PATH"
     fi
 else
-    BACKUP_PATH=$2
+    BACKUP_PATH="$2"
 fi
 
 if [ ! -d "$BACKUP_PATH" ]; then
@@ -43,34 +43,51 @@ if [ ! -w "$BACKUP_PATH" ]; then
     exit 1
 fi
 
-cd $PATH_TO_ESPO
+cd "$PATH_TO_ESPO"
 
 if [ ! -f "data/config.php" ]; then
     echo "Error: The '$PATH_TO_ESPO' is not EspoCRM directory."
     exit 1
 fi
 
-DB_USER=$(php -r "\$config=include('data/config.php'); echo \$config['database']['user'];")
-DB_PASS=$(php -r "\$config=include('data/config.php'); echo \$config['database']['password'];")
-DB_NAME=$(php -r "\$config=include('data/config.php'); echo \$config['database']['dbname'];")
+DB_NAME=$(php -r "\$config=include('data/config.php'); echo @\$config['database']['dbname'];")
+DB_USER=$(php -r "\$config=include('data/config.php'); echo @\$config['database']['user'];")
+DB_PASS=$(php -r "\$config=include('data/config.php'); echo @\$config['database']['password'];")
 
-BACKUP_NAME=$(basename $PATH_TO_ESPO)
+if [ -z "$DB_NAME" ]; then
+    DB_NAME=$(php -r "\$config=include('data/config-internal.php'); echo @\$config['database']['dbname'];")
+fi
+
+if [ -z "$DB_USER" ]; then
+    DB_USER=$(php -r "\$config=include('data/config-internal.php'); echo @\$config['database']['user'];")
+fi
+
+if [ -z "$DB_PASS" ]; then
+    DB_PASS=$(php -r "\$config=include('data/config-internal.php'); echo @\$config['database']['password'];")
+fi
+
+if [ -z "$DB_NAME" ]; then
+    echo "Error: Unable to determine database name."
+    exit 1
+fi
+
+BACKUP_NAME=$(basename "$PATH_TO_ESPO")
 BACKUP_ARCHIVE_NAME="$(date +'%Y-%m-%d_%H%M%S').tar.gz"
 
-cd $BACKUP_PATH
-mkdir $BACKUP_NAME
-cd $BACKUP_NAME
+cd "$BACKUP_PATH"
+mkdir -p "$BACKUP_NAME"
+cd "$BACKUP_NAME"
 
-# Create database dump
-mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > "db.sql" || {
+# Create database backup
+mysqldump --user="$DB_USER" --password="$DB_PASS" "$DB_NAME" > "db.sql" || {
     echo "Enter MySQL user:"
     read DB_USER
 
     echo "Enter MySQL password:"
     read DB_PASS
 
-    mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > "db.sql" || {
-        echo "Error: Cannot dump the database '$DB_NAME'. "
+    mysqldump --user="$DB_USER" --password="$DB_PASS" "$DB_NAME" > "db.sql" || {
+        echo "Error: Unable to create a backup of the database '$DB_NAME'. "
         exit 1
     }
 }
@@ -79,13 +96,13 @@ tar -czf "db.tar.gz" "db.sql"
 rm "db.sql"
 
 # Archive files
-tar -czf "files.tar.gz" -C $PATH_TO_ESPO .
+tar -czf "files.tar.gz" -C "$PATH_TO_ESPO" .
 
 # Create a full backup archive
 cd ..
-tar czf "$BACKUP_ARCHIVE_NAME" $BACKUP_NAME/
+tar czf "$BACKUP_ARCHIVE_NAME" "$BACKUP_NAME"/
 
 # Remove temporary files
-rm -rf $BACKUP_NAME
+rm -rf "$BACKUP_NAME"
 
-echo "Backup is created, '$BACKUP_PATH/$BACKUP_ARCHIVE_NAME'."
+echo "Backup is created at '$BACKUP_PATH/$BACKUP_ARCHIVE_NAME'."
